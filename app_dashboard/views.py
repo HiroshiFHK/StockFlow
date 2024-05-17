312c62799f56204124e950001407b6905a1762a3
Nathan Nery <nbnery@gmail.com>
2024-05-17 07:50:50 -0300

    Primeiro Grafico

--- app_dashboard/views.py
+++ app_dashboard/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.conf import settings
+from PIL import Image 
 import pandas as pd
 import sqlite3
+import matplotlib.pyplot as plt
 
 # @login_required
 def user_dashboard(request):
@@ -18,10 +21,26 @@ def user_dashboard(request):
 
     # Data analysis
     html_tabela = df.to_html(index = False)
+    sql = """
+        select
+            max(fabricante) fabricantes,
+            sum(qtdproduto) soma_prod
+        from
+            app_products_products app
+        group by
+            fabricante
+            """
+    dados = pd.read_sql(sql, conection)
+    plt.bar(dados.fabricantes, dados.soma_prod)
+    plt.title("Gráfico de Colunas por Fabricantes")
+    caminho_arq = settings.BASE_DIR/ 'templates' / 'static'/'colunas.png'
+    print(caminho_arq)
+    plt.savefig(caminho_arq)
 
     # Prepare for HTML
     context = {
-        'tabela_html': html_tabela
+        'tabela_html': html_tabela,
+        'imagem_grafico': 'caminho_arq'
     }
 
     return render(request, 'apps/app_dashboard/dashboard.html', context)
